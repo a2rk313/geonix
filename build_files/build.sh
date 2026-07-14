eee11c492c95f42a16cacb195c6e7c54be79478a
A2RK/aurky <arkkhan603@gmail.com>
2026-07-14 14:44:55 +0500

    [FIX] Fake package names from removed.

--- build_files/build.sh
+++ build_files/build.sh
@@ -14,7 +14,7 @@ dnf5 install -y \
     proj proj-data \
     geos libgeotiff \
     qgis python3-qgis \
-    qgis-grass grass grass-gui \
+    qgis-grass grass \
     python3-shapely \
     python3-fiona \
     python3-pyproj \
@@ -28,7 +28,7 @@ dnf5 install -y \
     liblas \
     python3-h5py \
     gpsd gpsbabel \
-    saga saga-gui \
+    saga \
     python3-scikit-learn \
     python3-numpy \
     python3-pandas \
